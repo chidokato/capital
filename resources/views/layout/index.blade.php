4a68566152a845fae4e56eee80bbfe2303c8bcf9
chidokato <tuan.pn92@gmail.com>
2024-07-31 16:39:31 +0700

    sdf

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -57,6 +57,14 @@
 
 <link rel="stylesheet" href="frontend/css/loading.css"/>
 
+<!-- Admicro Tag Manager -->
+<script> (function(a, b, d, c, e) { a[c] = a[c] || [];
+a[c].push({ "atm.start": (new Date).getTime(), event: "atm.js" });
+a = b.getElementsByTagName(d)[0]; b = b.createElement(d); b.async = !0;
+b.src = "//deqik.com/tag/corejs/" + e + ".js"; a.parentNode.insertBefore(b, a)
+})(window, document, "script", "atmDataLayer", "ATMTGXHZ20QTR");</script>
+<!-- End Admicro Tag Manager -->
+
 </head>
 <body class="bg" id="style-1">
 
